bb3d5c2b04630baff588d951780b990267e9ad37
Vinícios Grama <vinyirun1@gmail.com>
2020-02-05 13:58:06 -0300

    add navigation dependencies

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView />
+    </>
+  );
+};
+
+export default App;
