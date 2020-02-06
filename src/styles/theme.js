888f6f11b267d8208c986815cd40ab2081bee2a6
Vinícios Grama <vinyirun1@gmail.com>
2020-02-06 19:48:32 -0300

    add theme colors

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export const dark = {
+  mode: 'DARK',
+  background: '#000',
+  card: '#2C2C2C',
+  text: '#fff',
+  border: '#4F4F4F',
+};
+
+export const light = {
+  mode: 'LIGHT',
+  background: '#fff',
+  card: '#F7F7F7',
+  text: '#000',
+  border: '#A0A0A0',
+};
