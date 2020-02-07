2b460271f2affc0435cadc06888df10708a49281
Vinícios Grama <vinyirun1@gmail.com>
2020-02-06 22:47:36 -0300

    init stack home

---
+++ src/routes/stackHome.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {useTheme} from '@react-navigation/native';
+
+import OpenDrawer from '../components/OpenDrawer';
+
+import {Feed} from '../screens';
+
+const Stack = createStackNavigator();
+
+const StackHome = ({navigation}) => {
+  const {colors} = useTheme();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Página Inicial"
+        component={Feed}
+        options={{
+          headerLeft: () => <OpenDrawer navigationProps={navigation} />,
+          headerStyle: {
+            backgroundColor: colors.background,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default StackHome;
