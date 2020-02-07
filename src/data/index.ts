9a7bf3370c55ecc6e76ee5a73bc9deac54d6cbeb
Vinícios Grama <vinyirun1@gmail.com>
2020-02-06 22:47:23 -0300

    add fictitious data

---
+++ src/data/index.ts
@@ -0,0 +1,77 @@
+export const tweets = [
+  {
+    id: 1,
+    name: 'Nayan Hebert',
+    username: 'snakeethanoic',
+    date: '10 min',
+    content:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    avatar: 'https://api.adorable.io/avatars/face/eyes4/nose3/mouth7/8e8895',
+    comments: 12,
+    retweets: 36,
+    likes: 175,
+  },
+  {
+    id: 2,
+    name: 'Caiden Munro',
+    username: 'mlEyAmpt',
+    date: '2 d',
+    content:
+      'Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
+    avatar: 'https://api.adorable.io/avatars/400/abott@adorable.io.png',
+    comments: 12,
+    retweets: 36,
+    likes: 175,
+    verified: true,
+  },
+  {
+    id: 3,
+    name: 'Riley-James Philip',
+    username: 'omazeaMB',
+    date: '10 min',
+    content:
+      'Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.',
+    avatar: 'https://api.adorable.io/avatars/285/teste',
+    comments: 12,
+    retweets: 36,
+    likes: 175,
+    verified: true,
+  },
+  {
+    id: 4,
+    name: 'Faust Janey',
+    date: '1h',
+    username: 'faustjaneyking',
+    content:
+      'Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.',
+    avatar: 'https://api.adorable.io/avatars/285/testes',
+    comments: 12,
+    retweets: 36,
+    likes: 175,
+  },
+  {
+    id: 5,
+    name: 'Stanley Watson',
+    username: 'jaCTICKw',
+    date: '4h',
+    content:
+      'Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. ',
+    avatar: 'https://api.adorable.io/avatars/285/testes%20dsa',
+    comments: 12,
+    retweets: 36,
+    likes: 175,
+    verified: true,
+  },
+  {
+    id: 6,
+    name: 'Pharrell Sheppard',
+    username: 'mpinATes',
+    date: '12 dez 19',
+    content:
+      'Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. ',
+    avatar: 'https://api.adorable.io/avatars/285/fl',
+    comments: 12,
+    retweets: 36,
+    likes: 175,
+  },
+];
