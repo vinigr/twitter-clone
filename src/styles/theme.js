d84cb82ab5dc4001d91b6bf8107889041633f821
Vinícios Grama <vinyirun1@gmail.com>
2020-02-07 21:31:04 -0300

    update theme colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,12 +4,14 @@ export const dark = {
   card: '#2C2C2C',
   text: '#fff',
   border: '#4F4F4F',
+  input: '#202327',
 };
 
 export const light = {
   mode: 'LIGHT',
   background: '#fff',
-  card: '#F7F7F7',
+  card: '#E3E3E3',
   text: '#000',
   border: '#A0A0A0',
+  input: '#E6ECF0',
 };
